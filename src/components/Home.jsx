49c98a9e42ddd29f764a63daf7e3ee2baf642b56
Matheus Boechat <matheusfgb@gmail.com>
2023-04-19 17:23:36 -0300

    req4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
   constructor() {
@@ -9,14 +10,26 @@ class Home extends Component {
     };
   }
 
+  async componentDidMount() {
+    const response = await getCategories();
+    this.setState({
+      prodList: response,
+    });
+  }
+
   render() {
     const { prodList } = this.state;
     const emptyList = prodList.length === 0;
-
     return (
       <section>
         <p data-testid="home-initial-message">
-          {emptyList ? 'Digite algum termo de pesquisa ou escolha uma categoria.' : ''}
+          {emptyList ? 'Digite algum termo de pesquisa ou escolha uma categoria.'
+            : prodList.map((category) => (
+              <label key={ category.id } htmlFor={ category.id } data-testid="category">
+                <input type="radio" name="categories" id={ category.id } />
+                {category.name}
+              </label>
+            ))}
         </p>
       </section>
     );
