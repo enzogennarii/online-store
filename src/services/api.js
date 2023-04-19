d67980e816ecd27eb3a38d97cf78de5a16edac3d
Matheus Boechat <matheusfgb@gmail.com>
2023-04-19 16:27:40 -0300

    req1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,19 @@
 export async function getCategories() {
-  // Implemente aqui
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+
+  const response = await fetch(API_URL);
+  const data = await response.json();
+
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?category=';
+
+  const response = await fetch(`${API_URL}${categoryId}&q=${query}`);
+  const data = await response.json();
+
+  return data;
 }
 
 export async function getProductById() {
