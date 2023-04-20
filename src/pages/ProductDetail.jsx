795f70e1d51fc9bef032e9747df60640e56820a2
Ariana Miranda <ariichazkel@gmail.com>
2023-04-20 13:11:41 -0300

    lint

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -8,18 +8,18 @@ class ProductDetail extends Component {
     if (!product) {
       return <div>Produto não encontrado</div>;
     }
-    
+
     return (
       <section className="product-detail-page">
         <img 
-          src={ product.thumbnail } 
-          alt={ product.title } 
+          src={ product.thumbnail }
+          alt={ product.title }
           data-testid="product-detail-image" 
         />
         <h3 data-testid="product-detail-name">{ product.title }</h3>
         <p data-testid="product-detail-price">{ product.price }</p>
-        <button 
-          onClick={() => this.props.addToCart(product)} 
+        <button
+          onClick={() => this.props.addToCart(product)}
           data-testid="add-to-cart-button"
         >
           Adicionar ao carrinho
