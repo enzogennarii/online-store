499a877ccc58198818f3f839b27ab07f73ffac04
Ariana Miranda <ariichazkel@gmail.com>
2023-04-20 13:36:54 -0300

    req7 ok

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,24 +2,48 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class ProductDetail extends Component {
-  render() {
-    const { product, addToCart } = this.props;
+  state = {
+    loading: true,
+    product: null,
+    error: null,
+  }
 
-    if (!product) {
-      return <div>Produto não encontrado</div>;
+  async componentDidMount() {
+    const { productId } = this.props.match.params;
+    const url = `https://api.mercadolibre.com/items/${productId}`;
+
+    try {
+      const response = await fetch(url);
+      const product = await response.json();
+      this.setState({ product, loading: false });
+    } catch (error) {
+      this.setState({ error, loading: false });
+    }
+  }
+
+  render() {
+    const { addToCart } = this.props;
+    const { product, loading, error } = this.state;
+
+    if (loading) {
+      return <div>Carregando...</div>;
+    }
+
+    if (error) {
+      return <div>Erro ao carregar produto: {error.message}</div>;
     }
 
     return (
       <section className="product-detail-page">
         <img
-          src={ product.thumbnail }
-          alt={ product.title }
+          src={product.thumbnail}
+          alt={product.title}
           data-testid="product-detail-image"
         />
-        <h3 data-testid="product-detail-name">{ product.title }</h3>
-        <p data-testid="product-detail-price">{ product.price }</p>
+        <h3 data-testid="product-detail-name">{product.title}</h3>
+        <p data-testid="product-detail-price">{product.price}</p>
         <button
-          onClick={ () => addToCart(product) }
+          onClick={() => addToCart(product)}
           data-testid="add-to-cart-button"
         >
           Adicionar ao carrinho
@@ -30,13 +54,12 @@ class ProductDetail extends Component {
 }
 
 ProductDetail.propTypes = {
-  product: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    thumbnail: PropTypes.string.isRequired,
-  }).isRequired,
   addToCart: PropTypes.func.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      productId: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default ProductDetail;
