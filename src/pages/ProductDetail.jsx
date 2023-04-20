637c49044d7679b253ab3e3439910ac60a8e9c14
Ariana Miranda <ariichazkel@gmail.com>
2023-04-20 13:13:19 -0300

    lint

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -11,10 +11,10 @@ class ProductDetail extends Component {
 
     return (
       <section className="product-detail-page">
-        <img 
+        <img
           src={ product.thumbnail }
           alt={ product.title }
-          data-testid="product-detail-image" 
+          data-testid="product-detail-image"
         />
         <h3 data-testid="product-detail-name">{ product.title }</h3>
         <p data-testid="product-detail-price">{ product.price }</p>
